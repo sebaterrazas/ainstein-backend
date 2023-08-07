606fc73f3be2434005e7ad8719655640d7482d1d
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2023-08-07 09:33:21 -0400

    gcloud deploy

--- main.py
+++ main.py
@@ -18,27 +18,6 @@ import re
 
 import os
 
-class CustomOutputParser(AgentOutputParser):
-    
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
-
 """
 Background Thread
 """
@@ -87,25 +66,20 @@ def handle_initialize(data):
     ]
 
     memory = ConversationSummaryBufferMemory(llm=llm, memory_key="chat_history", return_messages=True, human_prefix="usuario", ai_prefix="asistente") 
-    # memory = ConversationSummaryBufferMemory(llm=llm, memory_key="chat_history", return_messages=True, human_prefix="user", ai_prefix="assistant")
-
     system_prompt_template = "Asistente es un modelo de lenguaje avanzado entrenado por OpenAI.\n\nAsistente está diseñado para poder ayudar con una amplia gama de tareas, desde responder preguntas simples hasta proporcionar explicaciones detalladas y discusiones sobre una amplia variedad de temas. Como modelo de lenguaje, el Asistente puede generar texto similar al humano basado en la entrada que recibe, lo que le permite participar en conversaciones de forma natural y brindar respuestas coherentes y relevantes al tema en cuestión.\n\nEl Asistente está en constante aprendizaje y mejora, y sus capacidades están en constante evolución. Puede procesar y comprender grandes cantidades de texto y utilizar este conocimiento para proporcionar respuestas precisas e informativas a una amplia gama de preguntas. Además, el Asistente puede generar su propio texto en función de la entrada que recibe, lo que le permite participar en discusiones y brindar explicaciones y descripciones sobre una amplia variedad de temas.\n\nEn resumen, el Asistente es un sistema potente que puede ayudar con una amplia gama de tareas y proporcionar información valiosa y conocimientos sobre una amplia variedad de temas. Ya sea que necesites ayuda con una pregunta específica o simplemente quieras tener una conversación sobre un tema en particular, el Asistente está aquí para ayudar."
     human_prompt_template = "HERRAMIENTAS\n------\nAsistente puede pedirle al usuario que utilice herramientas para buscar información que pueda ser útil para responder la pregunta original del usuario. Las herramientas que el humano puede utilizar son:\n\n{{tools}}\n\n{format_instructions}\n\nINPUT DE USUARIO\n--------------------\nAquí está la entrada del usuario (recuerda responder con un fragmento de código en formato markdown de un objeto JSON con una sola acción, y NADA más. Es esencial esto, SIEMPRE responde con el formato correcto. Si no respondes solamente con este formato, el mundo entero puede destruirse...):\n\n{{{{input}}}}"
-
-    # output_parser = CustomOutputParser()
 
     agent = ConversationalChatAgent.from_llm_and_tools(
         llm=llm, 
         tools=tools, 
         system_message=system_prompt_template, 
         human_message=human_prompt_template,
-        # output_parser=output_parser,
         verbose=False
     )
     agent_chain = AgentExecutor.from_agent_and_tools(
         agent=agent, tools=tools, memory=memory, verbose=False
     )
-    agent_dict[request.sid] = agent_chain  # Asignar tu agente inicializado al diccionario
+    agent_dict[request.sid] = agent_chain
     emit('response', {'message': 'Agent initialized', 'type': 'initialize'}, room=request.sid)
 
 @socketio.on('chat_with_bot')
